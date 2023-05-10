e56d7b8e568fce20f3869920e660092fdd86c60d
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-05-10 01:01:21 -0300

    Conexão com o BD

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -1,9 +1,10 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 import type {CadastroRequisicao} from '../../types/CadastroRequisicao';
+import { UsuarioModel } from '../../models/UsuarioModel';
 
 const endpointCadastro = 
-    (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
+    async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
 
     if (req.method === 'POST'){
         const usuario = req.body as CadastroRequisicao;
@@ -22,7 +23,9 @@ const endpointCadastro =
             return res.status(400).json({erro : 'Senha inválida.'});
         }
 
-        return  res.status(200).json({msg : 'Dados corretos.'});
+        // salvar no banco de dados
+        await UsuarioModel.create(usuario);
+        return res.status(200).json({msg : 'Usuário cadastrado com sucesso.'});
     }
     return res.status(405).json({erro : 'Método informado não é válido.'})
     }
