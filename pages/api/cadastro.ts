90a851305a9f4e80e2673a612c1c886bcafb2115
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-05-19 19:49:53 -0300

    Conclusão de Schemas
    Criação do Schema e Model. Criptografando a senha
    Salvar usuário no banco
    Validação de duplicidade de usuário;

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -1,7 +1,9 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 import type {CadastroRequisicao} from '../../types/CadastroRequisicao';
-import { UsuarioModel } from '../../models/UsuarioModel';
+import {UsuarioModel} from '../../models/UsuarioModel';
+import {conectarMongoDB} from '../../middlewares/conectarMongoDB'
+import md5 from 'md5';
 
 const endpointCadastro = 
     async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
@@ -23,11 +25,22 @@ const endpointCadastro =
             return res.status(400).json({erro : 'Senha inválida.'});
         }
 
+        // validação se já existe usuário com o mesmo email
+        const usuariosComMesmoEmail = await UsuarioModel.find({email : usuario.email});
+        if(usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0){
+            return res.status(400).json({erro : 'Já existe uma conta com o email informado.'});
+        }
+        
         // salvar no banco de dados
-        await UsuarioModel.create(usuario);
+        const usuarioASerSalvo = {
+            nome : usuario.nome,
+            email : usuario.email,
+            senha : md5(usuario.senha)
+        }
+        await UsuarioModel.create(usuarioASerSalvo);
         return res.status(200).json({msg : 'Usuário cadastrado com sucesso.'});
     }
     return res.status(405).json({erro : 'Método informado não é válido.'})
     }
 
-    export default endpointCadastro;
+    export default conectarMongoDB(endpointCadastro);
