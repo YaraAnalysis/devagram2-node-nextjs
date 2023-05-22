b5c9f461759b16ec68b852eb84fd28f08b6cbd69
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-05-22 16:53:36 -0300

    integração jwt - versão inicial

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -3,18 +3,30 @@ import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 import md5 from 'md5';
 import { UsuarioModel } from '../../models/UsuarioModel';
+import jwt from 'jsonwebtoken';
 
 const endpointLogin = async (
     req : NextApiRequest,
-    res : NextApiResponse<RespostaPadraoMsg>
+    res : NextApiResponse<RespostaPadraoMsg | any>
 ) => {
+
+    const {MINHA_CHAVE_JWT} = process.env;
+    if(!MINHA_CHAVE_JWT){
+        return res.status(500).json({erro : 'ENV JWT não informada.'});
+    }
+
     if(req.method === 'POST'){
         const {login, senha} = req.body;
 
         const usuariosEncontrados = await UsuarioModel.find({email : login, senha : md5(senha)});
         if(usuariosEncontrados && usuariosEncontrados.length > 0){
             const usuarioEncontrado = usuariosEncontrados[0];
-            return res.status(200).json({msg : `Usuário ${usuarioEncontrado.nome} autenticado com sucesso.`});
+
+            const token = jwt.sign({_id : usuarioEncontrado._id}, MINHA_CHAVE_JWT);
+            return res.status(200).json({
+                nome : usuarioEncontrado.nome,
+                email : usuarioEncontrado.email,
+                token});
         }
         return res.status(400).json({erro : 'Usuário ou senha não encontrados.'});
     }
