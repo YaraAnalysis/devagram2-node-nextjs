d5245c8caaaf2037bec53209b6f76b96ea5448eb
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-05-23 01:08:30 -0300

    adicionando o  middleware de validação nos endpoints

---
+++ pages/api/usuario.ts
@@ -0,0 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from "next";;
+import {validarTokenJWT} from '../../middlewares/validarTokenJWT';
+
+const usuarioEndpoint = (req : NextApiRequest, res : NextApiResponse) => {
+
+    return res.status(200).json({msg : 'Usuário autenticado com sucesso.'});
+}
+
+export default validarTokenJWT(usuarioEndpoint);
