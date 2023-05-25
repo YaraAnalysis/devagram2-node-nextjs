1e41c204d7e630618c51bea533bfd1078da40020
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-05-25 19:50:46 -0300

    Alterando o endpoint de cadastro para receber form-data

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -4,13 +4,15 @@ import type {CadastroRequisicao} from '../../types/CadastroRequisicao';
 import {UsuarioModel} from '../../models/UsuarioModel';
 import {conectarMongoDB} from '../../middlewares/conectarMongoDB'
 import md5 from 'md5';
+import {upload, uploadImagemCosmic} from '../../services/uploadImagemCosmic';
+import nc from 'next-connect';
 
-const endpointCadastro = 
-    async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
-
-    if (req.method === 'POST'){
+const handler = nc()
+    .use(upload.single('file'))
+    .post(async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
+        console.log('cadastro endpoint', req.body);
         const usuario = req.body as CadastroRequisicao;
-
+    
         if(!usuario.nome || usuario.nome.length < 2){
             return res.status(400).json({erro : 'Nome inválido.'});
         } 
@@ -31,16 +33,24 @@ const endpointCadastro =
             return res.status(400).json({erro : 'Já existe uma conta com o email informado.'});
         }
         
+        // enviar a imagem do multer para o cosmic
+        const image = await uploadImagemCosmic(req);
+
         // salvar no banco de dados
         const usuarioASerSalvo = {
             nome : usuario.nome,
             email : usuario.email,
-            senha : md5(usuario.senha)
+            senha : md5(usuario.senha),
+            avatar : image?.media?.url
         }
         await UsuarioModel.create(usuarioASerSalvo);
         return res.status(200).json({msg : 'Usuário cadastrado com sucesso.'});
-    }
-    return res.status(405).json({erro : 'Método informado não é válido.'})
-    }
+});
 
-    export default conectarMongoDB(endpointCadastro);
+export const config = {
+    api: {
+        bodyParser : false
+    }
+}
+   
+    export default conectarMongoDB(handler);
