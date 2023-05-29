96a6832ae486062f2c71346fe693690c95a5c776
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-05-29 12:37:36 -0300

    Criação do endpoint de feed pelo usuário
    get do usuario logado e feed pelo id do user

---
+++ pages/api/feed.ts
@@ -0,0 +1,34 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
+import {validarTokenJWT} from '../../middlewares/validarTokenJWT';
+import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
+import { UsuarioModel } from "../../models/UsuarioModel";
+import { PublicacaoModel } from "../../models/PublicacaoModel";
+
+const feedEndpoint = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any>) =>{
+    try{
+        if(req.method === 'GET'){
+            // receber uma info do id do usuario do qual quero buscar o feed
+            if(req?.query?.userId){
+                // agora que tenho o id do usuario, 
+                // como eu valido se é um usuario valido
+                const usuario = await UsuarioModel.findById(req?.query?.userId);
+                if(!usuario){
+                    return res.status(400).json({erro : 'Usuário não encontrado.'});
+                }
+
+                // e como eu busco as publicações dele?
+                const publicacoes = await PublicacaoModel
+                    .find({idUsuario : usuario._id})
+                    .sort({data : -1});
+                
+                return res.status(200).json(publicacoes);
+            }
+        }
+        return res.status(405).json({erro : "Método informado não é válido."});
+    }catch(e){
+        console.log(e);
+    }
+    return res.status(400).json({erro : 'Não foi possível obter o feed.'});
+}
+export default validarTokenJWT(conectarMongoDB(feedEndpoint));
--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -1,9 +1,20 @@
-import type { NextApiRequest, NextApiResponse } from "next";;
+import type { NextApiRequest, NextApiResponse } from "next";
+import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 import {validarTokenJWT} from '../../middlewares/validarTokenJWT';
+import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
+import {UsuarioModel} from "../../models/UsuarioModel";
 
-const usuarioEndpoint = (req : NextApiRequest, res : NextApiResponse) => {
-
-    return res.status(200).json({msg : 'Usuário autenticado com sucesso.'});
-}
-
-export default validarTokenJWT(usuarioEndpoint);
+const usuarioEndpoint = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any>) => {
+    try{
+        // pegar os dados do usuário logado através do id
+        const {userId} = req?.query;
+        // buscar todos os dados do usuário
+        const usuario = await UsuarioModel.findById(userId);
+        usuario.senha = null;
+        return res.status(200).json(usuario);
+    }catch(e){
+        console.log(e);
+    }
+    return res.status(400).json({erro : 'Não foi possível obter dados do usuário.'});
+};
+export default validarTokenJWT(conectarMongoDB(usuarioEndpoint));
