15838ba5fb19a5283c4988f940f80e1452ad4eb6
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-06-06 00:27:39 -0300

    api like /deslike

--- pages/api/like.ts
+++ pages/api/like.ts
@@ -17,8 +17,7 @@ const likeEndpoint = async (req : NextApiRequest, res : NextApiResponse<Resposta
                 return res.status(400).json({erro : 'Publicação não encontrada.'});
             }
 
-            // id do usuario que tá curtindo
-            // vem de onde?
+            // id do usuario que tá curtindo, vem de onde?
             const {userId} = req?.query;
             const usuario = await UsuarioModel.findById(userId);
             if(!usuario){
@@ -26,10 +25,10 @@ const likeEndpoint = async (req : NextApiRequest, res : NextApiResponse<Resposta
             }
 
             // como vamos administrar os likes?
-            // se o index for > -1, sinal que ele já curte a foto
             const indexDoUsuarioNoLike = publicacao.likes.findIndex((e : any) => e.toString() === usuario._id.toString());
 
             if(indexDoUsuarioNoLike != -1){
+                // se o index for > -1, sinal que ele já curte a foto
                 publicacao.likes.splice(indexDoUsuarioNoLike, 1);
                 await PublicacaoModel.findByIdAndUpdate({_id : publicacao._id}, publicacao);
                 return res.status(200).json({msg : 'Publicação descurtida com sucesso.'});
@@ -41,12 +40,6 @@ const likeEndpoint = async (req : NextApiRequest, res : NextApiResponse<Resposta
             }
         }
         return res.status(405).json({erro : 'Método informado não é válido.'});
-
-        // o que precisamos para curtir/descurtir uma publicação?
-        
-        // 
-        
-        // contador de incremento/decremento
 
     }catch(e){
         console.log(e);
